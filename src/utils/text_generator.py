8b21d79172a83be2e5bba3752a6f90947219fde3
radu.chivereanu <radu.chivereanu@fep8.grid.pub.ro>
2023-11-18 14:27:11 +0200

    text generation

--- src/utils/text_generator.py
+++ src/utils/text_generator.py
@@ -1,38 +1,44 @@
 from transformers import pipeline
 import json
+import time
 import torch
 import joblib
 from src.datasets.amass import AMASS
-
-pipe = pipeline("text-generation", model="HuggingFaceH4/zephyr-7b-beta", torch_dtype=torch.bfloat16, device="cuda")
-
+import clip
+pipe = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.1", torch_dtype=torch.bfloat16, device="cuda")
+pipe.tokenizer.pad_token_id = pipe.model.config.eos_token_id
 
 def generate_text(prompt, inputs):
-    prompts = [prompt + input for input in inputs]
+    prompts = [prompt + inp + ' [/INST]' for inp in inputs]
     outputs = pipe(prompts, batch_size=8, max_new_tokens=512, do_sample=True, temperature=0.5)
-    return [output[0]["generated_text"] for output in outputs]
+    return [output[0]["generated_text"].split('[/INST]')[-1] for output in outputs]
 
 
 def create_dataset(path: str):
     splits = ['vald']
     clip_model, clip_preprocess = clip.load("ViT-B/32", device='cuda',
                                             jit=False)  # Must set jit=False for training
-    with open(f'../../experiments/{path}/prompt.txt') as f:
+    with open(f'experiments/{path}/prompt.txt') as f:
         prompt = f.read()
 
+    total_examples = 0
     for split in splits:
         inputs = []
-        data = AMASS(datapath='../../data/amass_db/babel_30fps_db.pt', clip_preprocess=clip_preprocess,
+        data = AMASS(datapath='data/amass_db/babel_30fps_db.pt', clip_preprocess=clip_preprocess,
                      split=split,
                      num_frames=60)
+        total_exampes += len(data)
         for i in range(len(data)):
-            if i == 3:
-                break
             inputs.append(data._get_item_data_index(i)['clip_text'])
 
-        print(generate_text(prompt, inputs))
-        joblib.dump(data, f'../../experiments/{path}/dataset_{split}.pt')
+        texts = generate_text(prompt, inputs)
+        for i in range(len(data)):
+            data.set_clip_text(i, texts[i])
 
+        joblib.dump(data, f'experiments/{path}/dataset_{split}.pt')
+    return total_examples
 
 if __name__ == '__main__':
-    create_dataset('exp1')
+    start = time.time()
+    count = create_dataset('exp1')
+    print(f'Data generated! For {count} examples it took {time.time() - start seconds}')
